2d91b7e84809be3427653b070a79f936ab2ca38b
Natanael de Souza Figueiredo <natanael.souza.figueiredo@gmail.com>
2024-04-16 20:35:27 -0300

    Primeiro commit

---
+++ pdf_ocr_converter.py
@@ -0,0 +1,42 @@
+import ocrmypdf
+import tempfile
+import os
+import shutil
+import time
+import sys
+
+
+def clear_screen():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+def main():
+    ocrmypdf.configure_logging(verbosity=ocrmypdf.Verbosity.quiet, progress_bar_friendly=False)
+    while True:
+        clear_screen()
+        print("CONVERSOR DE ARQUIVOS PDF PARA O MODELO OCR")
+        print()
+        input_pdf = input("Arraste e solte o arquivo PDF nesta janela e tecle ENTER:\n")
+        input_pdf = input_pdf.strip().replace('"', '')      # Remove spaces and quotes from the file path
+        result = -1
+        try:
+            result = ocrmypdf.ocr(input_pdf, input_pdf, language='por')
+        except KeyboardInterrupt:
+            raise   # Handle on level up
+        except Exception as e:
+            print(f"Erro ao converter o arquivo. Detalhes: {e}")
+        if result == 0:
+            print("Conversão realizada com sucesso!")
+        else:
+            print(f"Erro ao converter o arquivo. Código de erro: {result}")
+        sys.stdout.flush()
+        input("\nPressione ENTER para continuar...")
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nOperação cancelada pelo usuário.")
+        time.sleep(2)
